3bf5f8341aaa3c79faa714a7f9ed0740e747b8ba
shshouse <1839192968@qq.com>
2026-03-05 19:19:20 +0800

    增加缓存文件大小限制

--- vite.config.js
+++ vite.config.js
@@ -49,6 +49,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,mp3}'],
+        maximumFileSizeToCacheInBytes: 100 * 1024 * 1024,
         runtimeCaching: [
           {
             urlPattern: /\.mp4$/,
