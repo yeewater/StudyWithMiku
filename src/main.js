7ce9867b7a98053beea5b0d4fd612ba5936cff36
shshouse <1839192968@qq.com>
2026-06-08 01:43:13 +0800

    re

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+if (import.meta.env.VITE_ANALYTICS_URL && import.meta.env.VITE_ANALYTICS_ID) {
+  const script = document.createElement('script')
+  script.defer = true
+  script.src = import.meta.env.VITE_ANALYTICS_URL
+  script.dataset.websiteId = import.meta.env.VITE_ANALYTICS_ID
+  document.head.appendChild(script)
+}
+
 if (window.location.pathname!=='/'&&!window.location.pathname.includes('.')){
   window.location.replace('/')//自动回根！
 }
